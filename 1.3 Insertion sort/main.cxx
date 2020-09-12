cb62b4a50c03595072a557fbab08de30a2f1ac22
Semen Martynov <semen.martynov@gmail.com>
2020-09-12 16:10:34 +0300

    Insertion sort: implementation

--- 1.3 Insertion sort/main.cxx
+++ 1.3 Insertion sort/main.cxx
@@ -1,14 +1,49 @@
 #include <fstream>
+#include <iostream>
+#include <algorithm>
+#include <vector>
 
 int main(int argc, char **argv)
 {
     std::ifstream fin("input.txt");
     std::ofstream fout("output.txt");
 
-    int a;
-    int b;
-    fin >> a >> b;
-    fout << (a + b * static_cast<long long>(b)) << std::endl;
+    size_t nums_c;
+    fin >> nums_c;
 
-    return 0;
+    std::vector<int> nums(nums_c);
+    std::vector<size_t> poss(nums_c);
+
+    // read numbers
+    for (size_t i = 0; i != nums_c; ++i)
+    {
+        fin >> nums[i];
+    }
+
+    poss[0] = 1;
+    for (size_t i = 1; i < std::size(nums); ++i)
+    {
+        size_t j = i;
+        while (j > 0)
+        {
+            if (nums[j] < nums[j - 1])
+            {
+                std::swap(nums[j], nums[j - 1]);
+                --j;
+                continue;
+            }
+            break;
+        }
+        poss[i] = j + 1;
+    }
+
+    std::for_each(std::cbegin(poss), std::cend(poss), [&fout](auto const &val) {
+        fout << val << " ";
+    });
+    fout << std::endl;
+
+    std::for_each(std::cbegin(nums), std::cend(nums), [&fout](auto const &val) {
+        fout << val << " ";
+    });
+    fout << std::endl;
 }
