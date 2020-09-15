25d67458cc2d657f7c3fa24ddca44f2a90a73bb2
Semen Martynov <semen.martynov@gmail.com>
2020-09-15 19:53:04 +0300

    Naive solution (with the time limit issue)

---
+++ 2.2 Inversion counter/naive.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+    std::ifstream fin("input.txt");
+
+    size_t nums_c;
+    fin >> nums_c;
+
+    std::vector<int> nums;
+    nums.reserve(nums_c);
+
+    std::istream_iterator<int> eos;
+    std::istream_iterator<int> iis(fin);
+    std::copy(iis, eos, std::back_inserter(nums));
+
+    int c = 0;
+    for (size_t i = 0; i != nums.size(); ++i)
+    {
+        for (size_t j = i; j != nums.size(); ++j)
+        {
+            if (nums[i] > nums[j])
+            {
+                ++c;
+            }
+        }
+    }
+
+    std::ofstream fout("output.txt");
+    fout << c << std::endl;
+
+    return 0;
+}
